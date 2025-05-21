1ee3d3c050b2e3f3310b816e9851b482c423b5e6
daihoclaptrinh <daidqhe181335@fpt.edu.vn>
2025-05-21 11:12:19 +0700

    them 2 class nưa workschedule va contact

---
+++ src/java/Model/WorkSchedule.java
@@ -0,0 +1,93 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import java.util.Date;
+
+
+public class WorkSchedule {
+    private String id;
+    private Doctor doctor;
+    private Date workDate;
+    private Date startTime;
+    private Date endTime;
+    private Date createdAt;
+    private Date updatedAt;
+
+    // Nếu cần ánh xạ đối tượng bác sĩ, có thể thêm:
+    // private User doctor;
+
+    public WorkSchedule() {
+    }
+
+    public WorkSchedule(String id, Doctor doctor, Date workDate, Date startTime, Date endTime, Date createdAt, Date updatedAt) {
+        this.id = id;
+        this.doctor = doctor;
+        this.workDate = workDate;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+
+    public Date getWorkDate() {
+        return workDate;
+    }
+
+    public void setWorkDate(Date workDate) {
+        this.workDate = workDate;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    
+    
+}
